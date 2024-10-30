0f838075b370b3ed9f15544c0705387f2d94d9d0
bbbbb <bbbbb@DESKTOP-ELI79IJ>
2024-10-30 17:03:44 +0800

    增加了一个dll直接引用的测试工程

---
+++ WpfDllTest2/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DllSqlTest1.Interfaces;
+using DllSqlTest1.Repositories;
+
+namespace WpfDllTest2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            string connectionString = "Host=localhost;Username=postgres;Password=613;Database=CoinCodeTest2;";
+            var SqlRepositories = new SqlRepositories(connectionString);
+            var mainWindow = new MainWindow(SqlRepositories);
+            mainWindow.Show();
+        }
+    }
+}
---
+++ WpfDllTest2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+using DllSqlTest1.Interfaces;
+using DllSqlTest1.Repositories;
+
+namespace WpfDllTest2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ISqlRepositories _sqlRepositories;
+        public MainWindow(ISqlRepositories sqlRepositories)
+        {
+            InitializeComponent();
+            _sqlRepositories = sqlRepositories;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = "u6";
+            int priorityNum = _sqlRepositories.GetPriorityNum(username);
+            MessageBox.Show(priorityNum.ToString());
+        }
+    }
+}
